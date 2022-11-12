0daffc364d22003660d7666ef978787b88caf484
Exigoll <exigoll@bk.ru>
2022-11-13 01:10:47 +0500

    fead: adaptive header

--- src/js/modules/functions.js
+++ src/js/modules/functions.js
@@ -2,15 +2,37 @@
 export function isWebp() {
 	// Проверка поддержки webp
 	function testWebP(callback) {
-		let webP = new Image()
+		let webP = new Image();
 		webP.onload = webP.onerror = function () {
-			callback(webP.height == 2)
-		}
-		webP.src ='data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA'
+			callback(webP.height == 2);
+		};
+		webP.src =
+			"data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
 	}
 	// Добавление класса _webp или _no-webp для HTML
 	testWebP(function (support) {
-		let className = support == true ? 'webp' : 'no-webp'
-		document.documentElement.classList.add(className)
-	})
+		let className = support == true ? "webp" : "no-webp";
+		document.documentElement.classList.add(className);
+	});
+}
+
+/*Burger-menu*/
+const iconMenu = document.querySelector(".navigation-header__icon");
+const menuBody = document.querySelector(".menu-header__list");
+if (iconMenu) {
+	iconMenu.addEventListener("click", function (e) {
+		document.body.classList.toggle("_lock");
+		iconMenu.classList.toggle("_active");
+		menuBody.classList.toggle("_active");
+	});
+}
+
+/*Toggle active link for header-navigation*/
+const currentLocation = location.href;
+const menuItem = document.querySelectorAll("nav ul li a");
+const menuLength = menuItem.length;
+for (let i = 0; i < menuLength; i++) {
+	if (menuItem[i].href === currentLocation) {
+		menuItem[i].className = "menu-header__link--active";
+	}
 }
