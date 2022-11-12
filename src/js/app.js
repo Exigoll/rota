717a7e93a6f47a1b48250b3d55c044a34e7ea96f
Exigoll <exigoll@bk.ru>
2022-11-12 12:44:21 +0500

    feat: burger menu & cative class link for navigation

--- src/js/app.js
+++ src/js/app.js
@@ -2,3 +2,22 @@ import * as flsFunctions from "./modules/functions.js";
 import { swiper } from "./modules/swiper.js";
 
 flsFunctions.isWebp();
+
+const iconMenu = document.querySelector(".navigation-header__icon");
+const menuBody = document.querySelector(".menu-header__list");
+if (iconMenu) {
+	iconMenu.addEventListener("click", function (e) {
+		document.body.classList.toggle("_lock");
+		iconMenu.classList.toggle("_active");
+		menuBody.classList.toggle("_active");
+	});
+}
+
+const currentLocation = location.href;
+const menuItem = document.querySelectorAll("nav ul li a");
+const menuLength = menuItem.length;
+for (let i = 0; i < menuLength; i++) {
+	if (menuItem[i].href === currentLocation) {
+		menuItem[i].className = "menu-header__link--active";
+	}
+}
