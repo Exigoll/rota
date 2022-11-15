82760557c924ce286513b9c57f7c995c20be44a3
Exigoll <exigoll@bk.ru>
2022-11-16 00:48:58 +0500

    fix: plugin minimage

--- gulp/config/path.js
+++ gulp/config/path.js
@@ -1,15 +1,10 @@
-// Получаем имя папки проекта
 import * as nodePath from "path";
 const rootFolder = nodePath.basename(nodePath.resolve());
 
-// Путь к папке с результатом. Папка будет создаваться автоматически и файлы из исходной папки src будут обрабатываться и переноситься в папку dist
 const buildFolder = "./dist";
-// Путь к папке с исходными файлами
 const srcFolder = "./src";
 
-// Общий объект, в котором будет храниться вся информация о пути к файлам и папкам
 export const path = {
-	/*путь к папке /dist с результатом: ${buildFolder} - папка /dist; /files - папка с файлами*/
 	build: {
 		js: `${buildFolder}/js`,
 		css: `${buildFolder}/css/`,
@@ -18,9 +13,6 @@ export const path = {
 		fonts: `${buildFolder}/assets/fonts/`,
 		files: `${buildFolder}/files/`
 	},
-	/*путь к папке /src с исходными файлами которые нужно скопировать: ${srcFolder} - папка с исходниками;/files -
-	 папка с файлами; /** - проверяем файлы в любых вложенных папках внутри папки /files; /*.* - проверяет все файлы с
-	  любыми расширениями*/
 	src: {
 		js: `${srcFolder}/js/app.js`,
 		images: `${srcFolder}/assets/img/**/*.{jpg,jpeg,png,gif,webp}`,
@@ -35,11 +27,12 @@ export const path = {
 		scss: `${srcFolder}/scss/**/*.scss`,
 		html: `${srcFolder}/**/*.html`,
 		images: `${srcFolder}/assets/img/**/*.{jpg,jpeg,png,gif,webp,svg,ico}`,
+		svgicons: `${srcFolder}/assets/svgicons/*.svg`,
 		files: `${srcFolder}/files/**/*.*`
 	},
-	clean: buildFolder, // будет равна папке с результатом
-	buildFolder: buildFolder, // сама папка с результатом
-	srcFolder: srcFolder, // папка с исходниками
-	rootFolder: rootFolder, // название текущей папки проекта
-	ftp: `test` // для указания папки на удаленном FTP сервере
+	clean: buildFolder,
+	buildFolder: buildFolder,
+	srcFolder: srcFolder,
+	rootFolder: rootFolder,
+	ftp: `test`
 };
